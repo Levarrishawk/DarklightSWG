eeb988e5e4244312fbd9569f286428fa91a6a7e6
Skyyyr <skydeath227@yahoo.com>
2015-03-15 20:32:42 -0700

    Update melee1hHit3.lua

--- MMOCoreORB/bin/scripts/commands/melee1hHit3.lua
+++ MMOCoreORB/bin/scripts/commands/melee1hHit3.lua
@@ -51,7 +51,7 @@ Melee1hHit3Command = {
 	mindCostMultiplier = 0,
         accuracyBonus = 25,
 
-	poolsToDamage = HEALTH_ATTRIBUTE,
+	poolsToDamage = HEALTH,
 	
 	animationCRC = hashCode("combo_5a_light"),
 
