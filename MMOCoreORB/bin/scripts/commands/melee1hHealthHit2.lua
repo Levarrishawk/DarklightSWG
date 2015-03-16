78c221153db5575ef3bd9f828c65ba2973dd7dc2
Skyyyr <skydeath227@yahoo.com>
2015-03-15 21:01:15 -0700

    Update melee1hHealthHit2.lua
    _ATTRIBUTE

--- MMOCoreORB/bin/scripts/commands/melee1hHealthHit2.lua
+++ MMOCoreORB/bin/scripts/commands/melee1hHealthHit2.lua
@@ -51,7 +51,7 @@ Melee1hHealthHit2Command = {
 	mindCostMultiplier = 0,
         accuracyBonus = 25,
 
-	poolsToDamage = HEALTH,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("combo_3c_light"),
 
