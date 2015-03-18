d215f3da03b9c83a62150819bc20c0395a96d632
Skyyyr <skydeath227@yahoo.com>
2015-03-18 15:58:30 -0700

    Update knife_dagger.lua
    -Fixed syntax

--- MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_dagger.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_dagger.lua
@@ -126,8 +126,8 @@ object_weapon_melee_knife_knife_dagger = object_weapon_melee_knife_shared_knife_
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "null", "null", "null"},
 	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
-	experimentalMin = {0, 0, 14, 32, 3.9, 6, 750, 17, 17, 3, 17, 4, 0, 0, 0},
-	experimentalMax = {0, 0, 26, 59, 2.7, 12, 1500, 33, 33, 3, 33, 4, 0, 0, 0},
+	experimentalMin = {0, 0, 14, 32, 3.9, 6, 750, 17, 17, 3, 17, 4, 0, 150, 0},
+	experimentalMax = {0, 0, 26, 59, 2.7, 12, 1500, 33, 33, 3, 33, 4, 0, 150, 0},
 	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
