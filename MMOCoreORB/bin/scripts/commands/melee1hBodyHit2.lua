b62ffb71940c5e994bb080d17ce76031963610e2
Skyyyr <skydeath227@yahoo.com>
2015-03-18 16:23:33 -0700

    Update melee1hBodyHit2.lua
    -Removed Health/Mind cost
    -Lowered damage
    -Increased speed

--- MMOCoreORB/bin/scripts/commands/melee1hBodyHit2.lua
+++ MMOCoreORB/bin/scripts/commands/melee1hBodyHit2.lua
@@ -44,11 +44,11 @@
 Melee1hBodyHit2Command = {
         name = "melee1hbodyhit2",
 
-	damageMultiplier = 2.5,
-	speedMultiplier = 2.0,
-	healthCostMultiplier = 0.75,
-	actionCostMultiplier = 0.75,
-	mindCostMultiplier = 1.25,
+	damageMultiplier = 1.5,
+	speedMultiplier = 3,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 4,
+	mindCostMultiplier = 0,
         accuracyBonus = 25,
 
 	poolsToDamage = HEALTH_ATTRIBUTE,
