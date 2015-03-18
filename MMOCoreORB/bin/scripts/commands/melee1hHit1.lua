69d4aab8e0ab4dc0f0fa44fda17db4cda2d0e1be
Skyyyr <skydeath227@yahoo.com>
2015-03-18 16:44:15 -0700

    Update melee1hHit1.lua
    -Fixed damage pool
    -Removed Health/Mind cost
    -Lowered damage
    -Increased speed

--- MMOCoreORB/bin/scripts/commands/melee1hHit1.lua
+++ MMOCoreORB/bin/scripts/commands/melee1hHit1.lua
@@ -44,12 +44,13 @@
 Melee1hHit1Command = {
 	name = "melee1hhit1",
 
-	damageMultiplier = 2.5,
-	speedMultiplier = 1.5,
-	healthCostMultiplier = 0.5,
-	actionCostMultiplier = 0.5,
-	mindCostMultiplier = 0.625,
+	damageMultiplier = 1.25,
+	speedMultiplier = 3,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2,
+	mindCostMultiplier = 0,
         accuracyBonus = 25,
+        poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("counter_high_center_light"),
 
