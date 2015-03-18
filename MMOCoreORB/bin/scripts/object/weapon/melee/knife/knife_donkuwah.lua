e2aeb38647b0de550fb0ee2d7095ae68b8489ad9
Skyyyr <skydeath227@yahoo.com>
2015-03-17 18:11:30 -0700

    Update knife_donkuwah.lua

--- MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_donkuwah.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_donkuwah.lua
@@ -101,7 +101,7 @@ object_weapon_melee_knife_knife_donkuwah = object_weapon_melee_knife_shared_knif
 	
 	-- The values below are the default values.  To be used for blue frog objects primarily
 	healthAttackCost = 0,
-	actionAttackCost = 6,
+	actionAttackCost = 150,
 	mindAttackCost = 0,
 	forceCost = 0,
 	
