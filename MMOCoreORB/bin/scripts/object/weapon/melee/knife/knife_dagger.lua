6ac8adb893c905924b7f644b6c35e645e840c8ae
Skyyyr <skydeath227@yahoo.com>
2015-03-18 15:46:34 -0700

    Update knife_dagger.lua
    -Fixed SAC

--- MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_dagger.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_dagger.lua
@@ -121,15 +121,15 @@ object_weapon_melee_knife_knife_dagger = object_weapon_melee_knife_shared_knife_
 
 	woundsRatio = 9,
 
-	numberExperimentalProperties = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalProperties = {"XX", "XX", "SR", "SR", "SR", "SR", "SR", "SR", "SR", "XX", "SR", "XX"},
-	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null"},
-	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange"},
-	experimentalMin = {0, 0, 14, 32, 3.9, 6, 750, 17, 17, 3, 17, 4},
-	experimentalMax = {0, 0, 26, 59, 2.7, 12, 1500, 33, 33, 3, 33, 4},
-	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	numberExperimentalProperties = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	experimentalProperties = {"XX", "XX", "SR", "SR", "SR", "SR", "SR", "SR", "SR", "XX", "SR", "XX", "XX", "XX", "XX"},
+	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "expDamage", "expDamage", "expDamage", "expDamage", "exp_durability", "expRange", "expRange", "null", "expRange", "null", "null", "null", "null"},
+	experimentalSubGroupTitles = {"null", "null", "mindamage", "maxdamage", "attackspeed", "woundchance", "hitpoints", "zerorangemod", "maxrangemod", "midrange", "midrangemod", "maxrange", "attackhealthcost", "attackactioncost", "attackmindcost"},
+	experimentalMin = {0, 0, 14, 32, 3.9, 6, 750, 17, 17, 3, 17, 4, 0, 0, 0},
+	experimentalMax = {0, 0, 26, 59, 2.7, 12, 1500, 33, 33, 3, 33, 4, 0, 0, 0},
+	experimentalPrecision = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 }
 
 ObjectTemplates:addTemplate(object_weapon_melee_knife_knife_dagger, "object/weapon/melee/knife/knife_dagger.iff")
