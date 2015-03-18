09a0c3fd26db034f2af53878e0f1e8d611af1db4
Skyyyr <skydeath227@yahoo.com>
2015-03-17 17:31:34 -0700

    Update baton_stun.lua
    Added frenzy skill mod.

--- MMOCoreORB/bin/scripts/object/weapon/melee/baton/baton_stun.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/baton/baton_stun.lua
@@ -98,7 +98,7 @@ object_weapon_melee_baton_baton_stun = object_weapon_melee_baton_shared_baton_st
 	speedModifiers = { "onehandmelee_speed" },
 
 	-- Leave blank for now
-	damageModifiers = { },
+	damageModifiers = { "frenzy" },
 
 
 	-- The values below are the default values.  To be used for blue frog objects primarily"Stun Baton":"1H Melee":"0":"stun"
