64d119cc9cf9e1e318638f71631b41006c89c818
Skyyyr <skydeath227@yahoo.com>
2015-03-17 18:11:47 -0700

    Update knife_janta.lua

--- MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_janta.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/knife/knife_janta.lua
@@ -101,7 +101,7 @@ object_weapon_melee_knife_knife_janta = object_weapon_melee_knife_shared_knife_j
 	
 	-- The values below are the default values.  To be used for blue frog objects primarily
 	healthAttackCost = 0,
-	actionAttackCost = 15,
+	actionAttackCost = 150,
 	mindAttackCost = 0,
 	forceCost = 0,
 	
