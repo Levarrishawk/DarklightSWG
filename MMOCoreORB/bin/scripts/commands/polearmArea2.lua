390d47fb569d02a3b5c581d86d261dd93a42c8d1
Skyyyr <skydeath227@yahoo.com>
2015-03-19 16:35:15 -0700

    Update polearmArea2.lua

--- MMOCoreORB/bin/scripts/commands/polearmArea2.lua
+++ MMOCoreORB/bin/scripts/commands/polearmArea2.lua
@@ -49,7 +49,7 @@ PolearmArea2Command = {
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 3.5,
 	mindCostMultiplier = 0,
-	accuracyBonus = 10,
+	accuracyBonus = 30,
 
 	areaRange = 7,
 	areaAction = true,
