329b8b1d94ee78f651d6b80a1d43aa900cc61c48
Levarrishawk <levarrishawk@gmail.com>
2015-03-19 19:35:01 -0400

    Update emperors_hand.lua

--- MMOCoreORB/bin/scripts/mobile/faction/imperial/emperors_hand.lua
+++ MMOCoreORB/bin/scripts/mobile/faction/imperial/emperors_hand.lua
@@ -1,5 +1,6 @@
 emperors_hand = Creature:new {
 	objectName = "@mob/creature_names:emperors_hand",
+	customName = "Aralina Silk (Emperor's Hand)",
 	socialGroup = "imperial",
 	faction = "imperial",
 	level = 252,
@@ -28,8 +29,9 @@ emperors_hand = Creature:new {
 	templates = {
 	--	"object/mobile/dressed_emperors_hand_human_male_01.iff",
 	--	"object/mobile/dressed_emperors_hand_human_male_02.iff",
-		"object/mobile/dressed_emperors_hand_human_female_01.iff",
-	--	"object/mobile/dressed_emperors_hand_zabrak_male_01.iff"},
+	--	"object/mobile/dressed_emperors_hand_zabrak_male_01.iff",
+		"object/mobile/dressed_emperors_hand_human_female_01.iff"},
+
 	lootGroups = {
      	{
 			groups = {
