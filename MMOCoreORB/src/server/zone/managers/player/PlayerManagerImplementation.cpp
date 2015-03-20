f1d0c8ed915a69dcc4f98d427dcdc47843a53194
Levarrishawk <levarrishawk@gmail.com>
2015-03-19 20:34:50 -0400

    Update PlayerManagerImplementation.cpp

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -123,11 +123,11 @@
 #include "server/zone/objects/player/Races.h"
 #include "server/zone/objects/tangible/components/droid/DroidPlaybackModuleDataComponent.h"
 
-/*  GOTO line 688 for next portion to uncomment:  NGE Player BH system
+//  GOTO line 688 for next portion:  NGE Player BH system
 #include "server/zone/managers/visibility/VisibilityManager.h"
 #include "server/zone/objects/player/sui/callbacks/BountyHuntSuiCallback.h"
 #include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
-*/
+
 
 #include <iostream>
 
@@ -691,7 +691,7 @@ void PlayerManagerImplementation::killPlayer(TangibleObject* attacker, CreatureO
 
 	player->updateTimeOfDeath();
 	player->clearBuffs(true);
-	/*  NGE BH SYSTEM, COMMENTED OUT UNTIL READY
+	//  NGE BH SYSTEM, COMMENTED OUT UNTIL READY
 	if(attacker->isPlayerCreature())
 	{
 		ManagedReference<SuiInputBox*> input = new SuiInputBox(player, SuiWindowType::STRUCTURE_VENDOR_WITHDRAW);
@@ -702,7 +702,7 @@ void PlayerManagerImplementation::killPlayer(TangibleObject* attacker, CreatureO
 
 		player->getPlayerObject()->addSuiBox(input);
 		player->sendMessage(input->generateMessage());
-	} */
+	} 
 
 	if (attacker->getFaction() != 0) {
 		if (attacker->isPlayerCreature() || attacker->isPet()) {
