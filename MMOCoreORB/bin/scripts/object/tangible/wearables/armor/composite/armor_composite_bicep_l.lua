271ab7c83567475b00be346422dcfdbba6d4dda6
Skyyyr <skydeath227@yahoo.com>
2015-03-21 14:29:03 -0700

    Update armor_composite_bicep_l.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_bicep_l.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_bicep_l.lua
@@ -91,9 +91,7 @@ object_tangible_wearables_armor_composite_armor_composite_bicep_l = object_tangi
 				"object/mobile/vendor/zabrak_male.iff" },
 
 	-- Damage types in WeaponObject
-	vulnerability = STUN + LIGHTSABER,
-
-	specialResists = ELECTRICITY,
+	vulnerability = LIGHTSABER,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 0,
