f6e3242170b594942568c8d855c747da77999316
Skyyyr <skydeath227@yahoo.com>
2015-03-21 14:35:41 -0700

    Update baton_stun.lua

--- MMOCoreORB/bin/scripts/object/weapon/melee/baton/baton_stun.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/baton/baton_stun.lua
@@ -116,8 +116,8 @@ object_weapon_melee_baton_baton_stun = object_weapon_melee_baton_shared_baton_st
 	maxRange = 4,
 	maxRangeAccuracy = 0,
 
-	minDamage = 125,
-	maxDamage = 175,
+	minDamage = 325,
+	maxDamage = 475,
 
 	attackSpeed = 5,
 
