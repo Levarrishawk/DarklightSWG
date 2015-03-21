ae835f0a9719057dd9dbd9846dc47faf578ea77b
Skyyyr <skydeath227@yahoo.com>
2015-03-21 14:28:04 -0700

    Update armor_composite_chest_plate.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_chest_plate.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_chest_plate.lua
@@ -91,9 +91,8 @@ object_tangible_wearables_armor_composite_armor_composite_chest_plate = object_t
 				"object/mobile/vendor/zabrak_male.iff" },
 
 	-- Damage types in WeaponObject
-	vulnerability = STUN + LIGHTSABER,
+	vulnerability = LIGHTSABER,
 
-	specialResists = ELECTRICITY,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 0,
