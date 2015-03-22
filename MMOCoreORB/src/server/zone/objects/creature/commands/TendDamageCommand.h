65705d4eda2c2c94fe111a0caf981ee5797948d1
Skyyyr <skydeath227@yahoo.com>
2015-03-21 21:56:08 -0700

    Update TendDamageCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/TendDamageCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TendDamageCommand.h
@@ -55,13 +55,13 @@ public:
 			: TendCommand(name, server) {
 		effectName = "clienteffect/healing_healdamage.cef";
 
-		mindCost = 200;
-		mindWoundCost = 5;
+		mindCost = 0;
+		mindWoundCost = 0;
 
 		tendDamage = true;
 
-		healthHealed = 50;
-		actionHealed = 50;
+		healthHealed = 500;
+	//	actionHealed = 50;
 
 		//defaultTime = 5.0;
 		range = 6.0;
