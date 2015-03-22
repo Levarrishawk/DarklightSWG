1f1ea181fd630d66328dcda70107d3106108cd4d
Skyyyr <skydeath227@yahoo.com>
2015-03-22 13:00:19 -0700

    Update ithorian_sentinel_armor_boots.lua
    -Removed Encum

--- MMOCoreORB/bin/scripts/loot/items/armor/ithorian_sentinel_armor_boots.lua
+++ MMOCoreORB/bin/scripts/loot/items/armor/ithorian_sentinel_armor_boots.lua
@@ -7,9 +7,7 @@ ithorian_sentinel_armor_boots = {
 		{"armor_rating",1,1,0},
 		{"armor_effectiveness",8,39,10},
 		{"armor_integrity",30000, 55000,0},
-		{"armor_health_encumbrance",25,15,0},
-		{"armor_action_encumbrance",44,22,0},
-		{"armor_mind_encumbrance",25,15,0},
+
 	},
 	skillMods = {
 
