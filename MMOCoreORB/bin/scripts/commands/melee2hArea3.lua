8a294c853476d04648c41df948f3657ab703bcdc
Skyyyr <skydeath227@yahoo.com>
2015-03-22 14:35:26 -0700

    Update melee2hArea3.lua

--- MMOCoreORB/bin/scripts/commands/melee2hArea3.lua
+++ MMOCoreORB/bin/scripts/commands/melee2hArea3.lua
@@ -43,24 +43,15 @@
 
 Melee2hArea3Command = {
         name = "melee2harea3",
-	damageMultiplier = 3.0,
-	speedMultiplier = 2.5,
-	healthCostMultiplier = 1.5,
-	actionCostMultiplier = 2.5,
-	mindCostMultiplier = 1.5,
-        accuracyBonus = 10,
+	damageMultiplier = 5,
+	speedMultiplier = 4,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 4,
+	mindCostMultiplier = 0,
+        accuracyBonus = 40,
+        poolsToDamage = HEALTH_ATTRIBUTE,
 
 	stateEffects = {
-	  StateEffect( 
-               --documentation clearly defines dizzy instead of blind
-		DIZZY_EFFECT, 
-		{}, 
-		{ "dizzy_defense", "resistance_states" }, 
-		{ "jedi_state_defense" }, 
-		30, 
-		100, 
-		30
-	  ),
 	  StateEffect( 
 		POSTUREDOWN_EFFECT, 
 		{ "postureDownRecovery" }, 
