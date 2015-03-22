3dfc225d72525f3d82780220e0ca8f57cd7fcc4c
Skyyyr <skydeath227@yahoo.com>
2015-03-22 16:30:32 -0700

    Update polearmHit3.lua

--- MMOCoreORB/bin/scripts/commands/polearmHit3.lua
+++ MMOCoreORB/bin/scripts/commands/polearmHit3.lua
@@ -43,10 +43,10 @@
 
 PolearmHit3Command = {
         name = "polearmhit3",
-	damageMultiplier = 5,
+	damageMultiplier = 5.5,
 	speedMultiplier = 3,
 	healthCostMultiplier = 0,
-	actionCostMultiplier = 3.5,
+	actionCostMultiplier = 3,
 	mindCostMultiplier = 0,
 	accuracyBonus = 30,
 	poolsToDamage = HEALTH_ATTRIBUTE,
