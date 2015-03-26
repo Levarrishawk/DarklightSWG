38fbbb03de2ff29c0deba9d225c63d6e9bed6daa
Skyyyr <skydeath227@yahoo.com>
2015-03-26 10:36:21 -0700

    Update armor_layer_heat.lua
    -Removed encumbrance

--- MMOCoreORB/bin/scripts/object/tangible/component/armor/armor_layer_heat.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/armor/armor_layer_heat.lua
@@ -45,11 +45,11 @@ object_tangible_component_armor_armor_layer_heat = object_tangible_component_arm
 
 
 	numberExperimentalProperties = {1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 1},
-	experimentalProperties = {"XX", "XX", "XX", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "OQ", "XX", "XX", "OQ", "SR", "XX"},
+	experimentalProperties = {"XX", "XX", "XX", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "XX", "XX", "XX", "XX", "OQ", "SR", "XX"},
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "exp_durability", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
+	experimentalGroupTitles = {"null", "null", "exp_durability", "exp_quality", "exp_durability", "null", "null", "null", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 1000, 1, 100, 20, 20, 20, 1, 32, 1, 20},
+	experimentalMin = {0, 0, 1000, 1, 100, 0, 0, 0, 1, 32, 1, 20},
 	experimentalMax = {0, 0, 1000, 5, 1000, 0, 0, 0, 1, 32, 15, 50},
 	experimentalPrecision = {0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
