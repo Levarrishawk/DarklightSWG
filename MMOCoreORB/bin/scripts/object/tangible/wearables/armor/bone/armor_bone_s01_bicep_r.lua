f3df5b124aaf368021e06b07a97b747716a723ec
Skyyyr <skydeath227@yahoo.com>
2015-03-26 11:18:55 -0700

    Update armor_bone_s01_bicep_r.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bone/armor_bone_s01_bicep_r.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/bone/armor_bone_s01_bicep_r.lua
@@ -88,7 +88,7 @@ object_tangible_wearables_armor_bone_armor_bone_s01_bicep_r = object_tangible_we
 				"object/mobile/vendor/zabrak_male.iff" },
 
 	-- kineticeffectiveness, energyeffectiveness, electricaleffectiveness, stuneffectiveness, blasteffectiveness, heateffectiveness, coldeffectiveness, acideffectiveness, restraineffectiveness
-	vulnerability = KINETIC + BLAST + STUN + HEAT + COLD + ACID + LIGHTSABER,
+	vulnerability = LIGHTSABER,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 6,
