fe8d25081278713964d9b77855c37081ca9052f3
Skyyyr <skydeath227@yahoo.com>
2015-03-25 17:28:16 -0700

    Update 2h_sword_battleaxe_quest.lua
    -Removed health/mind

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_battleaxe_quest.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_battleaxe_quest.lua
@@ -100,9 +100,9 @@ object_weapon_melee_2h_sword_2h_sword_battleaxe_quest = object_weapon_melee_2h_s
 	       
 	
 	-- The values below are the default values.  To be used for blue frog objects primarily
-	healthAttackCost = 33,
-	actionAttackCost = 11,
-	mindAttackCost = 6,
+	healthAttackCost = 0,
+	actionAttackCost = 150,
+	mindAttackCost = 0,
 	forceCost = 0,
 	
 	pointBlankAccuracy = 0,
