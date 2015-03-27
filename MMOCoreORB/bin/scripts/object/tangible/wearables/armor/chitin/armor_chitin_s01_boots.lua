2f98b7f3b3b941938a9f46d1768d4a0c7fbb9bcc
Skyyyr <skydeath227@yahoo.com>
2015-03-26 20:42:42 -0700

    Update armor_chitin_s01_boots.lua
    -Removed encumbrance
    -Removed normal vulnerabilities

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/chitin/armor_chitin_s01_boots.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/chitin/armor_chitin_s01_boots.lua
@@ -84,7 +84,7 @@ object_tangible_wearables_armor_chitin_armor_chitin_s01_boots = object_tangible_
 				"object/mobile/vendor/zabrak_male.iff" },
 
 	-- Damagetypes in WeaponObject
-	vulnerability = STUN + HEAT + COLD + ELECTRICITY + LIGHTSABER,
+	vulnerability = LIGHTSABER,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 13,
@@ -105,12 +105,12 @@ object_tangible_wearables_armor_chitin_armor_chitin_s01_boots = object_tangible_
 	lightSaber = 0,
 
 	numberExperimentalProperties = {1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1},
-	experimentalProperties = {"XX", "XX", "XX", "OQ", "SR", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "OQ", "XX", "XX", "OQ", "SR", "XX"},
+	experimentalProperties = {"XX", "XX", "XX", "OQ", "SR", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "XX", "XX", "XX", "OQ", "SR", "XX"},
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null", "null", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
+	experimentalGroupTitles = {"null", "null", "null", "null", "exp_quality", "exp_durability", "null", "null", "null", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 0, 1000, 5, 15000, 13, 25, 16, 1, 1, 5, 15000},
-	experimentalMax = {0, 0, 0, 1000, 30, 25000, 8, 15, 9, 1, 1, 40, 25000},
+	experimentalMin = {0, 0, 0, 1000, 5, 15000, 0, 0, 0, 1, 1, 5, 15000},
+	experimentalMax = {0, 0, 0, 1000, 30, 25000, 0, 0, 0, 1, 1, 40, 25000},
 	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
