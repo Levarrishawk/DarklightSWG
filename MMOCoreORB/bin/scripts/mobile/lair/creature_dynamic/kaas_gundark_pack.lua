111a488dd60298455523d5abd1401d56ce85ed48
Levarrishawk <levarrishawk@gmail.com>
2015-03-31 01:23:26 -0400

    Create kaas_gundark_pack.lua

---
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/kaas_gundark_pack.lua
@@ -0,0 +1,12 @@
+kaas_gundark_pack = Lair:new {
+	mobiles = {{"gundark",2},{"gundark",1}},
+	spawnLimit = 25,
+	buildingsVeryEasy = {},
+	buildingsEasy = {},
+	buildingsMedium = {},
+	buildingsHard = {},
+	buildingsVeryHard = {},
+	buildingType = "none"
+}
+
+addLairTemplate("kaas_gundark_pack", kaas_gundark_pack)
