be47e6dbe9b7faeb5b42dd7d4b9cd3a818087073
Levarrishawk <levarrishawk@gmail.com>
2015-03-31 05:03:55 -0400

    Update cripplingShot.lua

--- MMOCoreORB/bin/scripts/commands/cripplingShot.lua
+++ MMOCoreORB/bin/scripts/commands/cripplingShot.lua
@@ -44,10 +44,10 @@
 CripplingShotCommand = {
         name = "cripplingshot",
 
-	damageMultiplier = 15.0,
-	speedMultiplier = 4.0,
+	damageMultiplier = 5.0,
+	speedMultiplier = 2.0,
 	healthCostMultiplier = 0,
-	actionCostMultiplier = 16.0,
+	actionCostMultiplier = 3.0,
 	mindCostMultiplier = 0,
 	accuracyBonus = 25,
 
@@ -61,4 +61,3 @@ CripplingShotCommand = {
 }
 
 AddCommand(CripplingShotCommand)
-
