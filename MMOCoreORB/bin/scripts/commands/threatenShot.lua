27059669e6c0b8a912430e0caf9cb3bd50ce2141
Levarrishawk <levarrishawk@gmail.com>
2015-03-31 05:26:30 -0400

    Update threatenShot.lua

--- MMOCoreORB/bin/scripts/commands/threatenShot.lua
+++ MMOCoreORB/bin/scripts/commands/threatenShot.lua
@@ -46,12 +46,26 @@ ThreatenShotCommand = {
 
 	damageMultiplier = 0.25,
 	speedMultiplier = 2,
-	healthCostMultiplier = 1,
-	actionCostMultiplier = 1,
-	mindCostMultiplier = 1,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 3,
+	mindCostMultiplier = 0,
 	accuracyBonus = 15,
 
 	animationCRC = hashCode("fire_1_special_single_light"),
+	
+	poolsToDamage = HEALTH_ATTRIBUTE,
+	
+	stateEffects = {
+	  StateEffect( 
+		INTIMIDATE_EFFECT, 
+		{}, 
+		{ "intimidate_defense", "resistance_states" }, 
+		{ "jedi_state_defense" }, 
+		100, 
+		0, 
+		120 
+	  )
+	},
 
 	combatSpam = "threatenshot",
 
