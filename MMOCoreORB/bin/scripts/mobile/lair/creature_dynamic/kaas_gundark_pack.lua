48f2e684c035e13dd81d82516ecd9c52b7547e4e
Levarrishawk <levarrishawk@gmail.com>
2015-03-31 01:48:40 -0400

    Update kaas_gundark_pack.lua

--- MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/kaas_gundark_pack.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/kaas_gundark_pack.lua
@@ -1,11 +1,11 @@
 kaas_gundark_pack = Lair:new {
 	mobiles = {{"gundark",2},{"gundark",1}},
-	spawnLimit = 25,
-	buildingsVeryEasy = {},
-	buildingsEasy = {},
-	buildingsMedium = {},
-	buildingsHard = {},
-	buildingsVeryHard = {},
+	spawnLimit = 35,
+	buildingsVeryEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
+	buildingsEasy = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
+	buildingsMedium = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
+	buildingsHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
+	buildingsVeryHard = {"object/tangible/lair/base/poi_all_lair_rocks_large_evil_fire_red.iff"},
 	buildingType = "none"
 }
 
