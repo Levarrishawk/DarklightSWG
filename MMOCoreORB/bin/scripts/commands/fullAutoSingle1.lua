f4ae567ff0f2f2f9ef4d38b0a8d815e2e71c83bd
Levarrishawk <levarrishawk@gmail.com>
2015-03-31 05:06:44 -0400

    Update fullAutoSingle1.lua

--- MMOCoreORB/bin/scripts/commands/fullAutoSingle1.lua
+++ MMOCoreORB/bin/scripts/commands/fullAutoSingle1.lua
@@ -46,9 +46,9 @@ FullAutoSingle1Command = {
 
 	damageMultiplier = 2,
 	speedMultiplier = 1.5,
-	healthCostMultiplier = 1.75,
-	actionCostMultiplier = 2.5,
-	mindCostMultiplier = 0.5,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 4.75,
+	mindCostMultiplier = 0,
 	accuracyBonus = 25,
 
 	animationCRC = hashCode("fire_5_special_single_medium"),
@@ -85,7 +85,7 @@ FullAutoSingle1Command = {
 	  )
 	},
 
-	poolsToDamage = RANDOM_ATTRIBUTE,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	range = -1
 }
