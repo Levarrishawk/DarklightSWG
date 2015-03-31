aec509be0fe325a1feb557d9accce25f63523cf7
Levarrishawk <levarrishawk@gmail.com>
2015-03-30 20:36:04 -0400

    Create kaas_world.lua

---
+++ MMOCoreORB/bin/scripts/mobile/spawn/kaas_world.lua
@@ -0,0 +1,61 @@
+kaas_world = {
+
+	lairSpawns = {
+		{
+			lairTemplateName = "dantooine_dark_side_savage_neutral_none",
+			spawnLimit = 5,
+			minDifficulty = 75,
+			maxDifficulty = 79,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 25
+		},
+		{
+			lairTemplateName = "dantooine_force_renegade_neutral_none",
+			spawnLimit = 10,
+			minDifficulty = 80,
+			maxDifficulty = 84,
+			numberToSpawn = 0,
+			weighting = 6,
+			size = 25
+		},
+		{
+			lairTemplateName = "dantooine_force_archaist_neutral_none",
+			spawnLimit = 5,
+			minDifficulty = 90,
+			maxDifficulty = 94,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 25
+		},
+		{
+			lairTemplateName = "dantooine_force_crypt_crawler_neutral_none",
+			spawnLimit = 5,
+			minDifficulty = 95,
+			maxDifficulty = 99,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 25
+		},
+		{
+			lairTemplateName = "dantooine_force_wilder_neutral_none",
+			spawnLimit = 5,
+			minDifficulty = 115,
+			maxDifficulty = 119,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 25
+		},
+		{
+			lairTemplateName = "dantooine_force_crystal_hunter_neutral_none",
+			spawnLimit = 5,
+			minDifficulty = 115,
+			maxDifficulty = 119,
+			numberToSpawn = 0,
+			weighting = 3,
+			size = 25
+		},
+	}
+}
+
+addSpawnGroup("kaas_world", kaas_world);
