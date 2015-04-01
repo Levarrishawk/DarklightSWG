dde2f53f0b5e900f12f5315797376f6ba2cfca40
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 04:39:14 -0400

    Update fanShot.lua

--- MMOCoreORB/bin/scripts/commands/fanShot.lua
+++ MMOCoreORB/bin/scripts/commands/fanShot.lua
@@ -45,16 +45,16 @@ FanShotCommand = {
         name = "fanshot",
 	damageMultiplier = 4.0,
 	speedMultiplier = 2.0,
-	healthCostMultiplier = 0.5,
-	actionCostMultiplier = 1,
-	mindCostMultiplier = 0.5,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2.0,
+	mindCostMultiplier = 0,
         accuracyBonus = 50,
 
         --cone angle isn't strictly documented, leaving it alone
 	coneAngle = 60,
 	coneAction = true,
 
-	poolsToDamage = RANDOM_ATTRIBUTE,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("fire_area_light"),
 
