dbe2d5e7aa0866492c3b3bd1aa25e588d63e9e16
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 05:17:52 -0400

    Update fireAcidSingle2.lua

--- MMOCoreORB/bin/scripts/commands/fireAcidSingle2.lua
+++ MMOCoreORB/bin/scripts/commands/fireAcidSingle2.lua
@@ -46,12 +46,12 @@ FireAcidSingle2Command = {
 
 	damageMultiplier = 8,
 	speedMultiplier = 4,
-	healthCostMultiplier = 2.0,
-	actionCostMultiplier = 0.5,
-	mindCostMultiplier = 0.5,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 3.0,
+	mindCostMultiplier = 0,
 	accuracyBonus = 0,
 
-	poolsToDamage = RANDOM_ATTRIBUTE,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("fire_acid_rifle_single_2_light"),
 
