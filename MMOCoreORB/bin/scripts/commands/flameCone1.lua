f082af7b7b9940c87b2ff9ff8ccd29ee59bdce70
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 05:19:07 -0400

    Update flameCone1.lua

--- MMOCoreORB/bin/scripts/commands/flameCone1.lua
+++ MMOCoreORB/bin/scripts/commands/flameCone1.lua
@@ -46,15 +46,15 @@ FlameCone1Command = {
 
 	damageMultiplier = 5,
 	speedMultiplier = 4,
-	healthCostMultiplier = 1.5,
-	actionCostMultiplier = 0.5,
-	mindCostMultiplier = 0.5,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2.5,
+	mindCostMultiplier = 0,
 	accuracyBonus = 0,
 
 	coneAngle = 45,
 	coneAction = true,
 
-	poolsToDamage = RANDOM_ATTRIBUTE,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("fire_flame_thrower_cone_1_light"),
 	combatSpam = "flamecone1",
