5e2f95b2883133c607e4ef554d971defa5b61389
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 03:53:07 -0400

    Update flushingShot1.lua

--- MMOCoreORB/bin/scripts/commands/flushingShot1.lua
+++ MMOCoreORB/bin/scripts/commands/flushingShot1.lua
@@ -46,9 +46,9 @@ FlushingShot1Command = {
 
 	damageMultiplier = 2.0,
 	speedMultiplier = 2.0,
-	healthCostMultiplier = 0.5,
-	actionCostMultiplier = 0.5,
-	mindCostMultiplier = 1.5,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2.5,
+	mindCostMultiplier = 0,
 	accuracyBonus = 5,
 
 	stateEffects = {
@@ -72,7 +72,7 @@ FlushingShot1Command = {
 	  )
 	},
 
-	poolsToDamage = RANDOM_ATTRIBUTE,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("fire_3_special_single_medium"),
 
