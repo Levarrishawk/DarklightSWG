0abf7957667d9dc9efb85633dc8402ade338d640
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 04:19:16 -0400

    Update strafeShot2.lua

--- MMOCoreORB/bin/scripts/commands/strafeShot2.lua
+++ MMOCoreORB/bin/scripts/commands/strafeShot2.lua
@@ -46,15 +46,15 @@ StrafeShot2Command = {
 
 	damageMultiplier = 5.0,
 	speedMultiplier = 2.0,
-	healthCostMultiplier = 0.5,
-	actionCostMultiplier = 0.5,
-	mindCostMultiplier = 2.0,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 3.0,
+	mindCostMultiplier = 0,
 	accuracyBonus = 5,
 
 	coneAngle = 60,
 	coneAction = true,
 
-	poolsToDamage = RANDOM_ATTRIBUTE,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("fire_area_medium"),
 
