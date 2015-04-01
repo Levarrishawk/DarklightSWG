a133f274fe2c6925fea9c114d0716931d52a2945
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 03:40:12 -0400

    Update rifle_t21.lua

--- MMOCoreORB/bin/scripts/object/weapon/ranged/rifle/rifle_t21.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/rifle/rifle_t21.lua
@@ -100,9 +100,9 @@ object_weapon_ranged_rifle_rifle_t21 = object_weapon_ranged_rifle_shared_rifle_t
 
 
 	-- The values below are the default values.  To be used for blue frog objects primarily
-	healthAttackCost = 35,
-	actionAttackCost = 30,
-	mindAttackCost = 64,
+	healthAttackCost = 0,
+	actionAttackCost = 160,
+	mindAttackCost = 0,
 	forceCost = 0,
 
 	pointBlankRange = 0,
