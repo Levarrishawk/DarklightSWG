d754e527fe8f8ce75fa73c364ba2cb0aa970aaac
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 06:05:33 -0400

    Update rifle_acid_beam.lua

--- MMOCoreORB/bin/scripts/object/weapon/ranged/rifle/rifle_acid_beam.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/rifle/rifle_acid_beam.lua
@@ -112,8 +112,8 @@ object_weapon_ranged_rifle_rifle_acid_beam = object_weapon_ranged_rifle_shared_r
 	maxRange = 48,
 	maxRangeAccuracy = -120,
 
-	minDamage = 430,
-	maxDamage = 830,
+	minDamage = 130,
+	maxDamage = 230,
 
 	attackSpeed = 5.2,
 
