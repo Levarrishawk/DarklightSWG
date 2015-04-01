3d6f5389b95b554fd234f0f8894d4b1576e6988a
Levarrishawk <levarrishawk@gmail.com>
2015-04-01 05:24:12 -0400

    Update fireHeavyWeapon.lua

--- MMOCoreORB/bin/scripts/commands/fireHeavyWeapon.lua
+++ MMOCoreORB/bin/scripts/commands/fireHeavyWeapon.lua
@@ -46,15 +46,15 @@ FireHeavyWeaponCommand = {
 
 	damageMultiplier = 1.0,
 	speedMultiplier = 1.0,
-	healthCostMultiplier = 1.0,
-	actionCostMultiplier = 1.0,
-	mindCostMultiplier = 1.0,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 3.0,
+	mindCostMultiplier = 0,
 
 	range = 64,
 	areaAction = true,
 	areaRange = 10,
 
-	poolsToDamage = RANDOM,
+	poolsToDamage = HEALTH_ATTRIBUTE,
 }
 
 AddCommand(FireHeavyWeaponCommand)
