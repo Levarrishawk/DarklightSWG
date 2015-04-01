822c9094462a0599f49448b949a42366dcf8143e
Skyyyr <skydeath227@yahoo.com>
2015-03-31 19:18:48 -0700

    Update terminal_character_builder.lua
    enabled food that is in testing take 2...

--- MMOCoreORB/bin/scripts/object/tangible/terminal/terminal_character_builder.lua
+++ MMOCoreORB/bin/scripts/object/tangible/terminal/terminal_character_builder.lua
@@ -1626,11 +1626,11 @@ object_tangible_terminal_terminal_character_builder = object_tangible_terminal_s
 					"Backpack 01", "object/tangible/wearables/backpack/backpack_s01.iff"
 				}
 			},
-			--"Consumables",
-			--{
-			--	"Desserts",
-			--	{
-					--"Air Cake", "object/tangible/food/crafted/dessert_air_cake.iff",
+			"Consumables",
+			{
+				"Desserts",
+				{
+					"Air Cake", "object/tangible/food/crafted/dessert_air_cake.iff",
 					--"Almond Kwevvu Crisp Munchies", "object/tangible/food/crafted/dessert_almond_kwevvu_crisp_munchies.iff",
 					--"Bantha Butter", "object/tangible/food/crafted/dessert_bantha_butter.iff",
 					--"Blap Biscuit", "object/tangible/food/crafted/dessert_blap_biscuit.iff",
@@ -1638,9 +1638,9 @@ object_tangible_terminal_terminal_character_builder = object_tangible_terminal_s
 					--"Bofa Treat", "object/tangible/food/crafted/dessert_bofa_treat.iff",
 					--"Cavaellin Creams", "object/tangible/food/crafted/dessert_cavaellin_creams.iff",
 					--"Chandad", "object/tangible/food/crafted/dessert_chandad.iff",
-					--"Citros Snow Cake", "object/tangible/food/crafted/dessert_citros_snow_cake.iff",
+					"Citros Snow Cake", "object/tangible/food/crafted/dessert_citros_snow_cake.iff",
 					--"Corellian Fried Icecream", "object/tangible/food/crafted/dessert_corellian_fried_icecream.iff",
-					--"Deneelian Fizz Pudding", "object/tangible/food/crafted/dessert_deneelian_fizz_pudding.iff",
+					"Deneelian Fizz Pudding", "object/tangible/food/crafted/dessert_deneelian_fizz_pudding.iff",
 					--"Dweezel", "object/tangible/food/crafted/dessert_dweezel.iff",
 					--"Felbar", "object/tangible/food/crafted/dessert_felbar.iff",
 					--"Glazed Glucose Pate", "object/tangible/food/crafted/dessert_glazed_glucose_pate.iff",
@@ -1662,7 +1662,7 @@ object_tangible_terminal_terminal_character_builder = object_tangible_terminal_s
 					--"Vagnerian Canape", "object/tangible/food/crafted/dessert_vagnerian_canape.iff",
 					--"Wedding Cake", "object/tangible/food/crafted/dessert_wedding_cake.iff",
 					--"Won Won", "object/tangible/food/crafted/dessert_won_won.iff",
-				--},
+				} --When all is enabled, add a comma.
 				--"Dishes",
 				--{
 					--"Ahrisa", "object/tangible/food/crafted/dish_ahrisa.iff",
@@ -1789,7 +1789,7 @@ object_tangible_terminal_terminal_character_builder = object_tangible_terminal_s
 					--"Thruster Head", "object/tangible/food/spice/spice_thruster_head.iff",
 					--"Yarrock", "object/tangible/food/spice/spice_yarrock.iff"
 				--}
-			--},
+			},
 			"Deeds",
 			{
 				"Vehicle Deeds",
