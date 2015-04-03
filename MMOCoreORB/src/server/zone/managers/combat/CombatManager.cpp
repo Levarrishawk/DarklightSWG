81138c8421d261a9aaa9edbd1d5bc5854d3da2cb
Skyyyr <skydeath227@yahoo.com>
2015-04-02 20:56:07 -0700

    Update CombatManager.cpp

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -727,7 +727,7 @@ float CombatManager::getDefenderToughnessModifier(CreatureObject* defender, int 
 
 	int jediToughness = defender->getSkillMod("unarmed_toughness");
 	if (damType != WeaponObject::LIGHTSABER && jediToughness > 0)
-		damage *= 1.f - (jediToughness / 200.f);
+		damage *= 1.f - (jediToughness / 80.f);
 
 	float foodMitigatedDamage = damage;
 	int foodBonus = defender->getSkillMod("mitigate_damage");
