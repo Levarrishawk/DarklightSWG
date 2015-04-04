8cee62079c21e71f92013b37d49110a9924edcb7
Skyyyr <skydeath227@yahoo.com>
2015-04-04 15:46:14 -0700

    Update LuaIntangibleObject.cpp
    -older version

--- MMOCoreORB/src/server/zone/objects/intangible/LuaIntangibleObject.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/LuaIntangibleObject.cpp
@@ -4,49 +4,27 @@
  *  Created on: 13/01/2012
  *      Author: victor
  */
- 
- /*
- * LuaIntangibleObject.cpp
- *
- *  Created on: 13/01/2012
- *      Author: victor
- */
- 
+
 #include "LuaIntangibleObject.h"
 
 const char LuaIntangibleObject::className[] = "LuaIntangibleObject";
 
 Luna<LuaIntangibleObject>::RegType LuaIntangibleObject::Register[] = {
 		{ "_setObject", &LuaIntangibleObject::_setObject },
-		{ "_getObject", &LuaSceneObject::_getObject },
+
 		{ 0, 0 }
 };
 
 
 LuaIntangibleObject::LuaIntangibleObject(lua_State *L) : LuaSceneObject(L) {
-#ifdef DYNAMIC_CAST_LUAOBJECTS
-	realObject = dynamic_cast<IntangibleObject*>(_getRealSceneObject());
-
-	assert(!_getRealSceneObject() || realObject != NULL);
-#else
-	realObject = reinterpret_cast<IntangibleObject*>(lua_touserdata(L, 1));
-#endif
+	realObject = (IntangibleObject*)lua_touserdata(L, 1);
 }
 
 LuaIntangibleObject::~LuaIntangibleObject() {
 }
 
 int LuaIntangibleObject::_setObject(lua_State* L) {
-	LuaSceneObject::_setObject(L);
-
-#ifdef DYNAMIC_CAST_LUAOBJECTS
-	realObject = dynamic_cast<IntangibleObject*>(_getRealSceneObject());
-
-	assert(!_getRealSceneObject() || realObject != NULL);
-#else
-	realObject = reinterpret_cast<IntangibleObject*>(lua_touserdata(L, -1));
-#endif
+	realObject = (IntangibleObject*)lua_touserdata(L, -1);
 
 	return 0;
 }
-
