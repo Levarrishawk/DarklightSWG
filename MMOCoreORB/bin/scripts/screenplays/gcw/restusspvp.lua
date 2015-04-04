a59a686acaf1663f425ad43e241ab40ba5574e14
Skyyyr <skydeath227@yahoo.com>
2015-04-03 18:56:20 -0700

    Create restusspvp.lua
    added restuss pvp zone.

---
+++ MMOCoreORB/bin/scripts/screenplays/gcw/restusspvp.lua
@@ -0,0 +1,35 @@
+restusspvp = ScreenPlay:new {
+  numberOfActs = 1,
+  questString = "",
+  states = {},
+  questdata = Object:new {
+    activePlayerName = "initial",
+    }
+  }
+  
+  registerScreenPlay("restusspvp", true)
+  
+  function restusspvp:start()
+    self:spawnActiveAreas()
+  end
+  
+  function restusspvp:spawnActiveAreas()
+    local pSpawnArea1 = spawnSceneObject("rori", "object/active_area.iff", 5318, 80, 5680, 0, 0, 0, 0, 0)
+    
+    if (pSpawnArea1 ~= nil) then
+        local activeArea1 = LuaActiveArea(pSpawnArea1)
+        activeArea1:setCellObjectID(0)
+        activeArea1:setRadius(365)
+        createObserver(ENTEREDAREA, "restusspvp", "notifySpawnArea1", pSpawnArea1)
+    end
+  end
+  
+  function restusspvp:notifySpawnArea1(pActiveArea1, pMovingObject)
+    local movingObject = LuaSceneObject(pMovingObject)
+    local player =LuaCreatureObject(pMovingObject)
+    
+    if (movingObject:isCreatureObject()) then
+      player:sendSystemMessage("You have entered the Restuss PvP zone!")
+      playerObject:setFactionStatus(2)
+    end
+  end
