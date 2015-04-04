464c3a36000d3eb44b0f5538d5bc7ec3191a976d
Skyyyr <skydeath227@yahoo.com>
2015-04-04 16:46:59 -0700

    Update pvp.lua

--- MMOCoreORB/bin/scripts/screenplays/gcw/pvp.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/pvp.lua
@@ -1,3 +1,5 @@
+local ObjectManager = require("managers.object.object_manager")
+
 pvp = ScreenPlay:new {
 	numberOfActs = 1,
   	questString = "pvp",
@@ -25,6 +27,35 @@ function pvp:spawnActiveAreas()
 end
   
 function pvp:notifySpawnArea(pActiveArea, pMovingObject)
+	if not SceneObject(pMovingObject):isCreatureObject() then
+		return 0
+	end
+
+	return ObjectManager.withCreatureObject(pMovingObject, function(player)
+		if (player:isAiAgent() and not AiAgent(pMovingObject):isPet()) then
+			return 0
+		end
+		if (playerObjectPointer ~= nil and player:isImperial() or player:isRebel()) then
+			local playerObject = LuaPlayerObject(playerObjectPointer)
+			playerObject:setFactionStatus(2)
+			player:sendSystemMessage("You have entered the Restuss PvP zone!")
+		else
+			player:sendSystemMessage("You must be Rebel or Imperial to enter the PvP zone!")
+			player:teleport(5298, 78, 6115, 0)
+		end
+
+		return 0
+	end)
+end
+
+
+
+
+
+
+
+
+--[[
 	local movingObject = LuaSceneObject(pMovingObject)
 	local player = LuaCreatureObject(pMovingObject)
 	local playerObjectPointer = player:getPlayerObject()
@@ -41,3 +72,4 @@ function pvp:notifySpawnArea(pActiveArea, pMovingObject)
 		end
 	end
 end
+]]--
