b5db66be8851301698ea36549691023c29f357d2
Skyyyr <skydeath227@yahoo.com>
2015-04-03 18:56:50 -0700

    Update screenplays.lua
    added restusspvp.lua

--- MMOCoreORB/bin/scripts/screenplays/screenplays.lua
+++ MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -30,6 +30,7 @@ includeFile("legacy/force_sensitive.lua")--test
 includeFile("gcw/newsnet.lua")
 includeFile("gcw/recruiters/recruiterConvoHandler.lua")
 includeFile("gcw/recruiters/recruiterScreenplay.lua")
+includeFile("gcw/restusspvp.lua")
 
 -- Dungeons
 includeFile("dungeon/death_watch_bunker/deathWatchBunker.lua")
