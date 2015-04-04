ecd0ab5b85bfd22a5cd86e895779c2b24e74a7d3
Skyyyr <skydeath227@yahoo.com>
2015-04-04 14:06:59 -0700

    Update LuaIntangibleObject.cpp

--- MMOCoreORB/src/server/zone/objects/intangible/LuaIntangibleObject.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/LuaIntangibleObject.cpp
@@ -11,47 +11,7 @@
  *  Created on: 13/01/2012
  *      Author: victor
  */
-
-#include "LuaIntangibleObject.h"
-
-const char LuaIntangibleObject::className[] = "LuaIntangibleObject";
-
-Luna<LuaIntangibleObject>::RegType LuaIntangibleObject::Register[] = {
-		{ "_setObject", &LuaIntangibleObject::_setObject },
-
-		{ 0, 0 }
-};
-
-
-LuaIntangibleObject::LuaIntangibleObject(lua_State *L) : LuaSceneObject(L) {
-	realObject = (IntangibleObject*)lua_touserdata(L, 1);
-}
-
-LuaIntangibleObject::~LuaIntangibleObject() {
-}
-
-int LuaIntangibleObject::_setObject(lua_State* L) {
-	realObject = (IntangibleObject*)lua_touserdata(L, -1);
-
-	return 0;
-}
-
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-/*
 #include "LuaIntangibleObject.h"
 
 const char LuaIntangibleObject::className[] = "LuaIntangibleObject";
@@ -89,4 +49,4 @@ int LuaIntangibleObject::_setObject(lua_State* L) {
 
 	return 0;
 }
-*/
+
