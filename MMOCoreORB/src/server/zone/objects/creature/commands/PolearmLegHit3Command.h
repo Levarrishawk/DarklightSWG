a8077a41d1327b1347a8c711f2f87c59c27ba193
Skyyyr <skydeath227@yahoo.com>
2015-04-05 10:55:10 -0700

    Update PolearmLegHit3Command.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/PolearmLegHit3Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/PolearmLegHit3Command.h
@@ -70,14 +70,10 @@ public:
 		if (!weapon->isPolearmWeaponObject()) {
 			return INVALIDWEAPON;
 		}
-		Reference<TangibleObject*> targetObject = server->getZoneServer()->getObject(target).castTo<TangibleObject*>();
 
-		if (targetObject == NULL || !targetObject->isCreatureObject())
-			return INVALIDTARGET;
-		
 		Reference<SceneObject*> object = server->getZoneServer()->getObject(target);
 		ManagedReference<CreatureObject*> creatureTarget = cast<CreatureObject*>( object.get());
-	/*
+	
 		if (creatureTarget == NULL)
 			return GENERALERROR;
 
@@ -85,7 +81,7 @@ public:
 			creature->sendSystemMessage("You are out of range.");
 			return GENERALERROR;
 		}
-	*/
+	
 		uint32 buffcrc = BuffCRC::FORCE_RANK_SUFFERING;
 		uint32 buffcrc2 = BuffCRC::FORCE_RANK_SUFFERING;
 
