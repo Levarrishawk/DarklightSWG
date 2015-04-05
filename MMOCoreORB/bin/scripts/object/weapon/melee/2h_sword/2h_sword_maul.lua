d8d67d271c1f94935083e5a873378ac86fba1562
Skyyyr <skydeath227@yahoo.com>
2015-04-05 13:58:53 -0700

    Update 2h_sword_maul.lua

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_maul.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/2h_sword_maul.lua
@@ -114,8 +114,8 @@ object_weapon_melee_2h_sword_2h_sword_maul = object_weapon_melee_2h_sword_shared
 	maxRange = 5,
 	maxRangeAccuracy = 0,
 
-	minDamage = 550,
-	maxDamage = 650,
+	minDamage = 300,
+	maxDamage = 450,
 
 	attackSpeed = 4,
 
