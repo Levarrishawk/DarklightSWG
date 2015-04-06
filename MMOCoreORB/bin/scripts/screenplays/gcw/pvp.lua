ea4ff9bab7a845a7be16e002cd12a8a6f0127d7c
Skyyyr <skydeath227@yahoo.com>
2015-04-06 14:22:55 -0700

    Update pvp.lua

--- MMOCoreORB/bin/scripts/screenplays/gcw/pvp.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/pvp.lua
@@ -23,9 +23,11 @@ function pvp:spawnActiveAreas()
 	        activeArea:setCellObjectID(0)
 	        activeArea:setRadius(365)
 	        createObserver(ENTEREDAREA, "pvp", "notifySpawnArea", pSpawnArea)
+	        createObserver(EXITEDAREA, "pvp", "notifySpawnAreaLeave", pSpawnArea)
 	    end
 end
-  
+ 
+--checks if player enters the zone, and what to do with them.
 function pvp:notifySpawnArea(pActiveArea, pMovingObject)
 	
 	if (not SceneObject(pMovingObject):isCreatureObject()) then
@@ -48,6 +50,7 @@ function pvp:notifySpawnArea(pActiveArea, pMovingObject)
 	end)
 end
 
+--Handles the setting of factional status
 function pvp:handlePvpZone(pPlayer)
 	ObjectManager.withCreatureAndPlayerObject(pPlayer, function(player, playerObject)
 		deleteData(player:getObjectID() .. ":changingFactionStatus")
@@ -58,41 +61,20 @@ function pvp:handlePvpZone(pPlayer)
 
 end
 
---[[
-##REF
-function recruiterScreenplay:handleGoCovert(pPlayer)
-	ObjectManager.withCreatureAndPlayerObject(pPlayer, function(player, playerObject)
-		deleteData(player:getObjectID() .. ":changingFactionStatus")
-		playerObject:setFactionStatus(1)
+--Simply sends a system message
+function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
+		
+	if (not SceneObject(pMovingObject):isCreatureObject()) then
+		return 0
+	end
+	
+	return ObjectManager.withCreatureObject(pMovingObject, function(player)
+		if (player:isAiAgent()) then
+			return 0
+		end
+	player:sendSendSystemMessage("You have left the PvP Zone.")
+	
+	return 0
+	
 	end)
 end
-
-		if (playerObjectPointer ~= nil and player:isImperial() or player:isRebel()) then
-			local playerObject = LuaPlayerObject(playerObjectPointer)
-			playerObject:setFactionStatus(2)
-			player:sendSystemMessage("You have entered the Restuss PvP zone!")
-		else
-			player:sendSystemMessage("You must be Rebel or Imperial to enter the PvP zone!")
-			player:teleport(5298, 78, 6115, 0)
-		end
-
-
-
-
-	local movingObject = LuaSceneObject(pMovingObject)
-	local player = LuaCreatureObject(pMovingObject)
-	local playerObjectPointer = player:getPlayerObject()
-	local playerObject = LuaPlayerObject(pMovingObject)
-    
-	if (movingObject:isCreatureObject()) then
-		if (playerObjectPointer ~= nil and player:isImperial() or player:isRebel()) then
-			local playerObject = LuaPlayerObject(playerObjectPointer)
-			playerObject:setFactionStatus(2)
-			player:sendSystemMessage("You have entered the Restuss PvP zone!")
-		else
-			player:sendSystemMessage("You must be Rebel or Imperial to enter the PvP zone!")
-			player:teleport(5298, 78, 6115, 0)
-		end
-	end
-end
-]]--
