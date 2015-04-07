8512dff2894a58985133ff70fb283f47bd772391
Levarrishawk <levarrishawk@gmail.com>
2015-04-07 06:10:06 -0400

    Update space_dungeon_star_destroyer.lua

--- MMOCoreORB/bin/scripts/object/building/general/space_dungeon_star_destroyer.lua
+++ MMOCoreORB/bin/scripts/object/building/general/space_dungeon_star_destroyer.lua
@@ -42,6 +42,23 @@
 
 
 object_building_general_space_dungeon_star_destroyer = object_building_general_shared_space_dungeon_star_destroyer:new {
+	planetMapCategory = "cloningfacility",
+
+	skillMods = {
+		{"private_medical_rating", 100}
+        },
+
+        childObjects = {
+             --   {templateFile = "object/tangible/terminal/terminal_insurance.iff", x = -5.08604, z = 0.125266, y = -3.48717, ox = 0, oy = 0.722557, oz = 0, ow = 0.691312, cellid = 6, containmentType = -1},
+             --   {templateFile = "object/tangible/terminal/terminal_cloning.iff", x = 0.0387871, z = 0.125265, y = -6, ox = 0, oy = -0.0595427, oz = 0, ow = 0.998226, cellid = 3, containmentType = -1}
+        },
+
+	spawningPoints = {
+		{ x = -7.75978, z = 175.522, y = -34.3437, ow = 0.897693, ox = 0, oz = 0, oy = -0.440621, cellid = 33 }
+		}, -- { x, z, y, ow, ox, oy, oz, cellid }
+	templateType = CLONINGBUILDING 
+
+
 
 }
 
