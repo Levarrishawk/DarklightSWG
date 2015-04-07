19f48310cb530ca72fc8923382ae2999a958c453
Levarrishawk <levarrishawk@gmail.com>
2015-04-06 22:07:01 -0400

    Update isd.lua

--- MMOCoreORB/bin/scripts/screenplays/isd/isd.lua
+++ MMOCoreORB/bin/scripts/screenplays/isd/isd.lua
@@ -18,5 +18,23 @@ function ISDScreenPlay:spawnMobiles()
   
 
   --Main Room First Fight
-  spawnMobile("dungeon2", "krix_swift", 300, -4.9, 173.8, 44.0, 33, 34673093)
+  spawnMobile("dungeon2", "krix_swift", 300, -0.2, 173.8, 27.0, 0, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 35.5, 181.3, 40.6, -90, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 35.5, 181.3, 36.4, -90, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 35.5, 181.3, 26.5, -90, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 35.1, 13.0, 40.6, -50, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 27.7, 181.3, 5.7, -50, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 20.6, 181.3, -1.3, -50, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 10.1, 181.3, -1.9, 0, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, 0.0, 181.3, -1.9, 0, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -9.9, 181.3, -1.9, 0, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -20.9, 181.3, -1.2, 45, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -27.9, 181.3, 5.8, 45, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -35.2, 181.3, 13.0, 45, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -35.5, 181.3, 26.5, 90, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -35.5, 181.3, 35.6, 90, 34673093)
+  spawnMobile("dungeon2", "blackguard_stormtrooper_commando", 300, -35.5, 181.3, 44.5, 90, 34673093)
+
+
+  
   end
