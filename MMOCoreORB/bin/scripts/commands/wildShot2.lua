824b9cbaabdb68e6d3d0cb1d8c6bba50a53f4e19
Skyyyr <skydeath227@yahoo.com>
2015-04-10 09:07:08 -0700

    Update wildShot2.lua

--- MMOCoreORB/bin/scripts/commands/wildShot2.lua
+++ MMOCoreORB/bin/scripts/commands/wildShot2.lua
@@ -44,10 +44,10 @@
 WildShot2Command = {
         name = "wildshot2",
 
-	damageMultiplier = 3.0,
+	damageMultiplier = 2.0,
 	speedMultiplier = 2.0,
 	healthCostMultiplier = 0,
-	actionCostMultiplier = 3.75,
+	actionCostMultiplier = 3.5,
 	mindCostMultiplier = 0,
 	--accuracyBonus = 25,
 
