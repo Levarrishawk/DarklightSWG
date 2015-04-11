65d9bdb6e71e4f31aafa5504108d9b84d38b0df6
Skyyyr <skydeath227@yahoo.com>
2015-04-10 17:56:51 -0700

    Update config.lua

--- MMOCoreORB/bin/conf/config.lua
+++ MMOCoreORB/bin/conf/config.lua
@@ -140,6 +140,7 @@ TrePath = "/home/swgemu/workspace/tre"
 
 TreFiles = {
 	"default_patch.tre",
+	"darklight_02_hotfix.tre",
 	"darklight_01_hotfix.tre",
 	"darklight_02s.tre",
 	"darklight_02.tre",
