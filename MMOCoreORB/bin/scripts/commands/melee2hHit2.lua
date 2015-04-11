9f89f00a47f160b2d6467ee293f84015b8256128
Skyyyr <skydeath227@yahoo.com>
2015-04-11 16:47:00 -0700

    Update melee2hHit2.lua

--- MMOCoreORB/bin/scripts/commands/melee2hHit2.lua
+++ MMOCoreORB/bin/scripts/commands/melee2hHit2.lua
@@ -43,12 +43,12 @@
 
 Melee2hHit2Command = {
         name = "melee2hhit2",
-	damageMultiplier = 3.0,
-	speedMultiplier = 2.0,
-	healthCostMultiplier = 1.0,
-	actionCostMultiplier = 1.5,
-	mindCostMultiplier = 1.0,
-        accuracyBonus = 10,
+        	
+	damageMultiplier = 2.5,
+	speedMultiplier = 3,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2.75,
+	mindCostMultiplier = 0,
 
 	animationCRC = hashCode("combo_2a_medium"),
 
