5296bed0224ed450083ace03ffccfe7d57640705
Skyyyr <skydeath227@yahoo.com>
2015-04-11 16:40:53 -0700

    Update melee1hSpinAttack1.lua

--- MMOCoreORB/bin/scripts/commands/melee1hSpinAttack1.lua
+++ MMOCoreORB/bin/scripts/commands/melee1hSpinAttack1.lua
@@ -44,12 +44,11 @@
 Melee1hSpinAttack1Command = {
 	name = "melee1hspinattack1",
 
-	damageMultiplier = 2.0,
-	speedMultiplier = 1.5,
-	healthCostMultiplier = 1.0,
-	actionCostMultiplier = 1.0,
-	mindCostMultiplier = 1.5,
-        accuracyBonus = 25,
+	damageMultiplier = 2.5,
+	speedMultiplier = 3.5,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2.5,
+	mindCostMultiplier = 0,
 
 	animationCRC = hashCode("attack_high_right_medium_2"),
 
