2af4a5a4d488d3fc0d8da7ec6b4557f7f9a5a2ed
Skyyyr <skydeath227@yahoo.com>
2015-04-11 19:31:46 -0700

    Update polearmLunge2.lua

--- MMOCoreORB/bin/scripts/commands/polearmLunge2.lua
+++ MMOCoreORB/bin/scripts/commands/polearmLunge2.lua
@@ -44,13 +44,13 @@
 PolearmLunge2Command = {
 	name = "polearmlunge2",
 
-	damageMultiplier = 4,
+	damageMultiplier = 2,
 	speedMultiplier = 4,
-	accuracyBonus = 30,
-	poolsToDamage = HEALTH_ATTRIBUTE,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 2.5,
 	mindCostMultiplier = 0,
+	
+	poolsToDamage = HEALTH_ATTRIBUTE,
 
 	animationCRC = hashCode("lower_posture_polearm_2"),
 
@@ -60,7 +60,7 @@ PolearmLunge2Command = {
 
 	stateEffects = {
 	  StateEffect( 
-		KNOCKDOWN_EFFECT, 
+		POSTUREDOWN_EFFECT, 
 		{ "knockdownRecovery", "lastKnockdown" }, 
 		{ "knockdown_defense" }, 
 		{}, 
