2829e2481dc263f448e8a3df50436a0f37dfafe9
Skyyyr <skydeath227@yahoo.com>
2015-04-12 17:27:59 -0700

    Update polearmHit2.lua

--- MMOCoreORB/bin/scripts/commands/polearmHit2.lua
+++ MMOCoreORB/bin/scripts/commands/polearmHit2.lua
@@ -45,7 +45,7 @@ PolearmHit2Command = {
         name = "polearmhit2",
 
 	damageMultiplier = 3.5,
-	speedMultiplier = 3,
+	speedMultiplier = 2,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 3,
 	mindCostMultiplier = 0,
