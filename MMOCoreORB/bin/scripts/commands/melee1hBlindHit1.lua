0d5a5a5743f6fd28cf12be2221000706ece5d2ee
Skyyyr <skydeath227@yahoo.com>
2015-04-12 17:22:59 -0700

    Update melee1hBlindHit1.lua

--- MMOCoreORB/bin/scripts/commands/melee1hBlindHit1.lua
+++ MMOCoreORB/bin/scripts/commands/melee1hBlindHit1.lua
@@ -45,7 +45,7 @@ Melee1hBlindHit1Command = {
         name = "melee1hblindhit1",
 
 	damageMultiplier = 1.75,
-	speedMultiplier = 3,
+	speedMultiplier = 1.5,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 2.5,
 	mindCostMultiplier = 0,
