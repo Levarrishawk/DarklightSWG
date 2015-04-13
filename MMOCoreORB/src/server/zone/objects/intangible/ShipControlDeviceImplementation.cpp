d67c9539173b8e98d4e93d07f5148237c84d8d9e
Skyyyr <skydeath227@yahoo.com>
2015-04-13 15:42:09 -0700

    Update ShipControlDeviceImplementation.cpp

--- MMOCoreORB/src/server/zone/objects/intangible/ShipControlDeviceImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/ShipControlDeviceImplementation.cpp
@@ -42,7 +42,6 @@ which carries forward this exception.
 */
 
 #include "server/zone/ZoneServer.h"
-#include "server/zone/ZoneProcessServer.h"
 #include "server/zone/objects/intangible/ShipControlDevice.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 #include "server/zone/objects/player/PlayerObject.h"
