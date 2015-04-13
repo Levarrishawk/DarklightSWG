264ac91e765328f2ab8f2626483c07ef24f93a9b
Skyyyr <skydeath227@yahoo.com>
2015-04-12 17:31:07 -0700

    Update unarmedStun1.lua

--- MMOCoreORB/bin/scripts/commands/unarmedStun1.lua
+++ MMOCoreORB/bin/scripts/commands/unarmedStun1.lua
@@ -45,9 +45,7 @@ UnarmedStun1Command = {
 	name = "unarmedstun1",
 
 	damageMultiplier = 2,
-	speedMultiplier = 2.0,
-	--accuracyBonus = 15,
-	
+	speedMultiplier = 1.0,
 	healthCostMultiplier = 0,
 	actionCostMultiplier = 3.5,
 	mindCostMultiplier = 0,
