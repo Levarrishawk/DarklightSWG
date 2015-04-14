66eb2deed076efb50a8a5ff36b60c7df6c30eab8
Levarrishawk <levarrishawk@gmail.com>
2015-04-14 01:14:52 -0400

    Update and rename kaas_gcw.lua to kaas_pvp.lua

--- MMOCoreORB/bin/scripts/screenplays/gcw/kaas_pvp.lua
+++ MMOCoreORB/bin/scripts/screenplays/gcw/kaas_pvp.lua
@@ -1,34 +1,34 @@
 local ObjectManager = require("managers.object.object_manager")
 
-pvp = ScreenPlay:new {
+kaas_pvp = ScreenPlay:new {
 	numberOfActs = 1,
-  	questString = "pvp",
+  	questString = "kaas_pvp",
   	states = {onleave = 1, overt = 2},
   	questdata = Object:new {
     	activePlayerName = "initial",
     	}
 }
   
-registerScreenPlay("pvp", true)
+registerScreenPlay("kaas_pvp", true)
   
-function pvp:start()
+function kaas_pvp:start()
     	self:spawnActiveAreas()
 end
   
-function pvp:spawnActiveAreas()
-	local pSpawnArea = spawnSceneObject("rori", "object/active_area.iff", 5318, 80, 5680, 0, 0, 0, 0, 0)
+function kaas_pvp:spawnActiveAreas()
+	local pSpawnArea = spawnSceneObject("rori", "object/active_area.iff", 0, 80, 0, 0, 0, 0, 0, 0)
     
 	if (pSpawnArea ~= nil) then
 		local activeArea = LuaActiveArea(pSpawnArea)
 	        activeArea:setCellObjectID(0)
-	        activeArea:setRadius(365)
-	        createObserver(ENTEREDAREA, "pvp", "notifySpawnArea", pSpawnArea)
-	        createObserver(EXITEDAREA, "pvp", "notifySpawnAreaLeave", pSpawnArea)
+	        activeArea:setRadius(7000)
+	        createObserver(ENTEREDAREA, "kaas_pvp", "notifySpawnArea", pSpawnArea)
+	        createObserver(EXITEDAREA, "kaas_pvp", "notifySpawnAreaLeave", pSpawnArea)
 	    end
 end
  
 --checks if player enters the zone, and what to do with them.
-function pvp:notifySpawnArea(pActiveArea, pMovingObject)
+function kaas_pvp:notifySpawnArea(pActiveArea, pMovingObject)
 	
 	if (not SceneObject(pMovingObject):isCreatureObject()) then
 		return 0
@@ -40,18 +40,18 @@ function pvp:notifySpawnArea(pActiveArea, pMovingObject)
 		end
 		
 		if (player:isImperial() or player:isRebel()) then
-			createEvent(1, "pvp", "handlePvpZone", pMovingObject)
-			player:sendSystemMessage("You have entered the Restuss PvP zone!")
+			createEvent(1, "kaas_pvp", "handlePvpZone", pMovingObject)
+			player:sendSystemMessage("You have arrived on Dromund Kaas.")
 		else
-			player:sendSystemMessage("You must be Rebel or Imperial to enter the PvP zone!")
-			player:teleport(5298, 78, 6115, 0)
+			player:sendSystemMessage("You must be part of a Faction to travel to Dromund Kaas!")
+			player:teleport(corellia, -137, 28, -4722, 95)
 		end
 		return 0
 	end)
 end
 
 --Handles the setting of factional status
-function pvp:handlePvpZone(pPlayer)
+function kaas_pvp:handlePvpZone(pPlayer)
 	ObjectManager.withCreatureAndPlayerObject(pPlayer, function(player, playerObject)
 		deleteData(player:getObjectID() .. ":changingFactionStatus")
 		if (playerObject:isCovert() or playerObject:isOnLeave()) then
@@ -62,7 +62,7 @@ function pvp:handlePvpZone(pPlayer)
 end
 
 --Simply sends a system message
-function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
+function kaas_pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
 	
 	if (not SceneObject(pMovingObject):isCreatureObject()) then
 		return 0
@@ -74,7 +74,7 @@ function pvp:notifySpawnAreaLeave(pActiveArea, pMovingObject)
 		end
 		
 		if (player:isImperial() or player:isRebel()) then
-			player:sendSystemMessage("You have left the Restuss PvP zone!")
+			player:sendSystemMessage("You have left Dromund Kaas")
 		end
 		return 0
 	end)
