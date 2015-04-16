c37660a372ccd0af22e39ad82bd5ade877c97962
Skyyyr <skydeath227@yahoo.com>
2015-04-15 20:35:33 -0700

    Update kaas_imperial_garrison.lua

--- MMOCoreORB/bin/scripts/screenplays/poi/kaas_imperial_garrison.lua
+++ MMOCoreORB/bin/scripts/screenplays/poi/kaas_imperial_garrison.lua
@@ -273,7 +273,7 @@ function kaas_imperial_garison:boss_damage(playerObject, creatureObject, damage)
 	health = boss:getHAM(0)
 	maxHealth = boss:getMaxHAM(0)
 
-	if (health <= (maxHealth * 0.9)) then print("checking HAM")
+	if (health <= (maxHealth * 0.75)) then print("checking HAM")
 		spatialChat(playerObject, "To my side apprentices!") print("spatial")
 		writeData("kaas_imperial_garison:spawnAdd", 1) print("writing data")
 		if (readData("kaas_imperial_garison:spawnAdd") == 1) then print("checking spawnAdd")
