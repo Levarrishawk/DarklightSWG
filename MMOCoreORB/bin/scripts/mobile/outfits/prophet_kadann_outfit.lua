ce9ce422d055e14e49f5f9cceb8697935f250435
Levarrishawk <levarrishawk@gmail.com>
2015-04-16 02:04:38 -0400

    Create prophet_kadann_outfit.lua

---
+++ MMOCoreORB/bin/scripts/mobile/outfits/prophet_kadann_outfit.lua
@@ -0,0 +1,8 @@
+prophet_kadann_outfit = {
+	{ 
+		{objectTemplate = "object/tangible/wearables/robe/exar_cultist_hood_up.iff", customizationVariables = {} },
+	}
+	 
+}
+
+addOutfitGroup("prophet_kadann_outfit", prophet_kadann_outfit)
