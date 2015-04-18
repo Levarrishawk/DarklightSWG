1d749c6054a71bb8480572e2fe445e8e05fbead9
Skyyyr <skydeath227@yahoo.com>
2015-04-18 10:09:22 -0700

    Update TendCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/TendCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TendCommand.h
@@ -268,7 +268,7 @@ public:
 		}
 
 		creature->inflictDamage(creature, CreatureAttribute::ACTION, mindCost, false);
-		awardXp(creature, "medical", 200;
+		awardXp(creature, "medical", 200);
 		//creature->addWounds(CreatureAttribute::FOCUS, mindWoundCost);
 		//creature->addWounds(CreatureAttribute::WILLPOWER, mindWoundCost);
 		//creature->addShockWounds(2);
