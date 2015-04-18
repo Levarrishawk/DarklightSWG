b9183a443288db740c4148fa21336e9e90ee461b
Skyyyr <skydeath227@yahoo.com>
2015-04-18 16:23:16 -0700

    Update BactaCureCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/BactaCureCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/BactaCureCommand.h
@@ -50,10 +50,27 @@ which carries forward this exception.
 #include "CombatQueueCommand.h"
 
 class BactaCureCommand : public CombatQueueCommand {
+	int mindCost;
+	int mindWoundCost;
+
+	int healthHealed;
+	int actionHealed;
+	int mindHealed;
+
+	float speed;
+	float range;
 public:
 
 	BactaCureCommand(const String& name, ZoneProcessServer* server)
 		: CombatQueueCommand(name, server) {
+		healthHealed = 0;
+		actionHealed = 0;
+		mindHealed = 0;
+
+		mindCost = 700;
+		mindWoundCost = 0;
+
+		range = 30;
 	}
 	
 	void doAnimations(CreatureObject* creature, CreatureObject* creatureTarget) {
