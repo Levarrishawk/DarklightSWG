537cb8e47f7e5642abe89e456528586e37bd94aa
Skyyyr <skydeath227@yahoo.com>
2015-04-18 16:20:57 -0700

    Update BactaCureCommand.h

--- MMOCoreORB/src/server/zone/objects/creature/commands/BactaCureCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/BactaCureCommand.h
@@ -55,7 +55,91 @@ public:
 	BactaCureCommand(const String& name, ZoneProcessServer* server)
 		: CombatQueueCommand(name, server) {
 	}
+	
+	void doAnimations(CreatureObject* creature, CreatureObject* creatureTarget) {
+		//creatureTarget->playEffect("clienteffect/bacta_jab.cef", "");
 
+		if (creature == creatureTarget)
+			creature->playEffect("clienteffect/bacta_jab.cef", "");
+		else
+			creature->playEffect("clienteffect/bacta_jab.cef", "");
+	}
+	bool checkTarget(CreatureObject* creature, CreatureObject* creatureTarget) {
+
+		if (!creatureTarget->hasState(CreatureState::STUNNED)) {
+			return false;
+		}		
+
+		if (!creatureTarget->hasState(CreatureState::DIZZY)) {
+			return false;
+		}
+
+		if (!creatureTarget->hasState(CreatureState::INTIMIDATED)) {
+			return false;
+		}
+		
+		if (!creatureTarget->hasState(CreatureState::BLINDED)) {
+			return false;
+		}
+
+		if (!creatureTarget->isPoisoned()) {
+			return false;
+		}
+
+		if (!creatureTarget->isDiseased()) {
+			return false;
+		}
+
+		if (!creatureTarget->isBleeding()) {
+			return false;
+		}	
+		
+		return true;
+	}
+	void deactivateInjuryTreatment(CreatureObject* creature) {
+	
+		int modSkill = (creature->getSkillMod("healing_range_speed") * 0.1);
+		
+		int delay = 15 - modSkill;
+		
+		StringIdChatParameter message("healing_response", "healing_response_58"); //You are now ready to heal more damage.
+		Reference<InjuryTreatmentTask*> task = new InjuryTreatmentTask(creature, message, "injuryTreatment");
+		creature->addPendingTask("injuryTreatment", task, delay * 1000);
+	}
+
+	void sendHealMessage(CreatureObject* creature, CreatureObject* creatureTarget, int healthDamage, int actionDamage) {
+		if (!creature->isPlayerCreature())
+			return;
+
+		CreatureObject* player = cast<CreatureObject*>(creature);
+
+		StringBuffer msgPlayer, msgTarget, msgBody, msgTail;
+
+		if (healthDamage > 0) {
+			msgBody << healthDamage << " health damage";
+		} else if (healthDamage > 0) {
+			msgBody << healthDamage << " health"; 
+		}else {
+			return; //No damage to heal.
+		}
+
+		msgTail << " damage.";
+
+		if (creature == creatureTarget) {
+			msgPlayer << "You heal yourself for " << msgBody.toString() << msgTail.toString();
+			player->sendSystemMessage(msgPlayer.toString());
+		} else if (creatureTarget->isPlayerCreature()){
+			msgPlayer << "You heal " << creatureTarget->getFirstName() << " for " << msgBody.toString() << msgTail.toString();
+			msgTarget << player->getFirstName() << " heals you for " << msgBody.toString() << msgTail.toString();
+
+			player->sendSystemMessage(msgPlayer.toString());
+			creatureTarget->sendSystemMessage(msgTarget.toString());
+		} else {
+			msgPlayer << "You heal " << creatureTarget->getDisplayedName() << " for " << msgBody.toString() << msgTail.toString();
+			player->sendSystemMessage(msgPlayer.toString());
+		}
+		
+	}
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
 
 		if (!checkStateMask(creature))
@@ -63,8 +147,103 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+		if (!creature->canTreatInjuries()) {
+			creature->sendSystemMessage("@healing_response:healing_must_wait"); //You must wait before you can do that.
+			return GENERALERROR;
+		}
+	
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
 
-		return doCombatAction(creature, target);
+		if (object != NULL) {
+			if (!object->isCreatureObject()) {
+				TangibleObject* tangibleObject = dynamic_cast<TangibleObject*>(object.get());
+
+				if (tangibleObject != NULL && tangibleObject->isAttackableBy(creature)) {
+					object = creature;
+				} else 
+					creature->sendSystemMessage("@healing_response:healing_response_99"); //Target must be a player or a creature pet in order to quick heal.
+					return GENERALERROR;
+			}
+		} else
+			object = creature;
+
+
+		CreatureObject* creatureTarget = cast<CreatureObject*>(object.get());
+
+		Locker clocker(creatureTarget, creature);
+
+		if ((creatureTarget->isAiAgent() && !creatureTarget->isPet()) || creatureTarget->isDroidObject() || creatureTarget->isDead() || creatureTarget->isRidingMount() || creatureTarget->isAttackableBy(creature))
+			creatureTarget = creature;
+
+		if (!creatureTarget->isInRange(creature, range + creatureTarget->getTemplateRadius() + creature->getTemplateRadius()))
+			return TOOFAR;
+
+		if (creature->isProne() || creature->isMeditating()) {
+			creature->sendSystemMessage("@error_message:wrong_state"); //You cannot complete that action while in your current state.
+			return GENERALERROR;
+		}
+
+		if (creature->isRidingMount()) {
+			creature->sendSystemMessage("@error_message:survey_on_mount"); //You cannot perform that action while mounted on a creature or driving a vehicle.
+			return GENERALERROR;
+		}
+		
+		if (!creatureTarget->isHealableBy(creature)) {
+			creature->sendSystemMessage("@healing:pvp_no_help");  //It would be unwise to help such a patient.
+			return GENERALERROR;
+		}
+
+		if (creature->getHAM(CreatureAttribute::ACTION) < mindCost) {
+			creature->sendSystemMessage("You do not have enough action to do that."); //You do not have enough mind to do that.
+			return GENERALERROR;
+		}
+		
+		if (!creatureTarget->hasDamage(CreatureAttribute::HEALTH)) {
+			if (creatureTarget == creature)
+				creature->sendSystemMessage("@healing_response:healing_response_61"); //You have no damage to heal.
+			else if (creatureTarget->isPlayerCreature()) {
+				StringIdChatParameter stringId("healing_response", "healing_response_63"); //%NT has no damage to heal.
+				stringId.setTT(creatureTarget->getObjectID());
+				creature->sendSystemMessage(stringId);
+			} else {
+				StringBuffer message;
+				message << creatureTarget->getDisplayedName() << " has no damage to heal.";
+				creature->sendSystemMessage(message.toString());
+			}
+
+			return GENERALERROR;
+		}
+		int healDisease = creatureTarget->healDot(CreatureState::DISEASED, 300);
+		int healPoison = creatureTarget->healDot(CreatureState::POISONED, 300);
+		int roll = System:random(4);
+		if (roll == 0)
+			creatureTarget->removeStateBuff(CreatureState::STUNNED);
+		else if (roll == 1)
+			creatureTarget->removeStateBuff(CreatureState::DIZZY);
+		else if (roll == 2)
+			creatureTarget->removeStateBuff(CreatureState::BLINDED);
+		else if (roll = 3)
+			creatureTarget->removeStateBuff(CreatureState::INTIMIDATED);
+
+		int healPower = (creature->getSkillMod("combat_healing_ability") * 3);
+
+		int healedHealth = creatureTarget->healDamage(creature, CreatureAttribute::HEALTH, healPower);
+		int healedAction = creatureTarget->healDamage(creature, CreatureAttribute::ACTION, healPower);
+
+		if (creature->isPlayerCreature()) {
+			PlayerManager* playerManager = server->getZoneServer()->getPlayerManager();
+			playerManager->sendBattleFatigueMessage(creature, creatureTarget);
+		}
+
+		sendHealMessage(creature, creatureTarget, healedHealth, healedAction);
+
+		creature->inflictDamage(creature, CreatureAttribute::ACTION, mindCost, false);
+		creature->addShockWounds(5);
+		
+ 		deactivateInjuryTreatment(creature);
+		doAnimations(creature, creatureTarget);
+
+		return SUCCESS;
 	}
 
 };
