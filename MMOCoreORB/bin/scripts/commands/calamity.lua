a4eaf460c9b2f6456c783306d8de8027743bfb35
Skyyyr <skydeath227@yahoo.com>
2015-04-19 10:51:34 -0700

    Update calamity.lua

--- MMOCoreORB/bin/scripts/commands/calamity.lua
+++ MMOCoreORB/bin/scripts/commands/calamity.lua
@@ -1,6 +1,17 @@
 CalamityCommand = {
         name = "calamity",
-
+        
+	damageMultiplier = 4,
+	speedMultiplier = 1.0,
+	healthCostMultiplier = 0,
+	actionCostMultiplier = 2,
+	mindCostMultiplier = 0,
+	
+	animationCRC = 0,
+	effectString = "clienteffect/calamity.cef",
+	poolsToDamage = HEALTH_ATTRIBUTE,
+	combatSpam = "calamity",
+	range = -1
 }
 
 AddCommand(CalamityCommand)
