cf9bb165cc71b37d241e198c1f82c7a6ea0f5a4d
Skyyyr <skydeath227@yahoo.com>
2015-04-19 16:35:13 -0700

    Update GuildMemberInfo.cpp

--- MMOCoreORB/src/server/zone/objects/guild/GuildMemberInfo.cpp
+++ MMOCoreORB/src/server/zone/objects/guild/GuildMemberInfo.cpp
@@ -1,9 +1,49 @@
+
 /*
  * GuildMemberInfo.cpp
  *
  *  Created on: Nov 4, 2010
  *      Author: crush
  */
+
+#include "GuildMemberInfo.h"
+#include "GuildObject.h"
+
+GuildMemberInfo::GuildMemberInfo(uint64 playerid) : Object() {
+	playerID = playerid;
+	permissions = GuildObject::PERMISSION_NONE;
+	declaredAllegiance = 0;
+
+	//addSerializableVariables();
+}
+
+int GuildMemberInfo::compareTo(const GuildMemberInfo& gmi) const {
+	if (playerID < gmi.playerID)
+		return 1;
+	else if (playerID > gmi.playerID)
+		return -1;
+	else
+		return 0;
+}
+
+GuildMemberInfo& GuildMemberInfo::operator=(const GuildMemberInfo& gmi) {
+	if (this == &gmi)
+		return *this;
+
+	playerID = gmi.playerID;
+	guildTitle = gmi.guildTitle;
+	permissions = gmi.permissions;
+	declaredAllegiance = gmi.declaredAllegiance;
+
+	return *this;
+}
+
+/*
+ * GuildMemberInfo.cpp
+ *
+ *  Created on: Nov 4, 2010
+ *      Author: crush
+ *
 
 #include "GuildMemberInfo.h"
 #include "server/zone/objects/guild/GuildObject.h"
@@ -36,3 +76,4 @@ GuildMemberInfo& GuildMemberInfo::operator=(const GuildMemberInfo& gmi) {
 
 	return *this;
 }
+*/
