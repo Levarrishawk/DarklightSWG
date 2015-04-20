34b44078da1acd50398ff7ba6bb3dcc18d010e24
Skyyyr <skydeath227@yahoo.com>
2015-04-19 17:32:02 -0700

    Update GuildMemberList.h

--- MMOCoreORB/src/server/zone/objects/guild/GuildMemberList.h
+++ MMOCoreORB/src/server/zone/objects/guild/GuildMemberList.h
@@ -1,3 +1,4 @@
+
 /*
  * GuildMemberList.h
  *
@@ -33,41 +34,3 @@ using namespace server::zone::objects::guild;
 
 
 #endif /* GUILDMEMBERLIST_H_ */
-
-
-
-/*
- * GuildMemberList.h
- *
- *  Created on: Oct 27, 2010
- *      Author: crush
- *
-
-#ifndef GUILDMEMBERLIST_H_
-#define GUILDMEMBERLIST_H_
-
-#include "engine/engine.h"
-
-#include "GuildMemberInfo.h"
-
-namespace server {
- namespace zone {
-  namespace objects {
-   namespace guild {
-
-	class GuildMemberList : public VectorMap<uint64, GuildMemberInfo> {
-		public:
-
-		GuildMemberList() {
-			setNoDuplicateInsertPlan();
-		}
-	};
-   }
-  }
- }
-}
-
-using namespace server::zone::objects::guild;
-
-
-#endif \* GUILDMEMBERLIST_H_ */
