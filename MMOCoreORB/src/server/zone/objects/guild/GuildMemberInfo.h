13aadd9eb03a1103fb9392c157f7a3b022ec5dbe
Skyyyr <skydeath227@yahoo.com>
2015-04-19 17:13:54 -0700

    Update GuildMemberInfo.h

--- MMOCoreORB/src/server/zone/objects/guild/GuildMemberInfo.h
+++ MMOCoreORB/src/server/zone/objects/guild/GuildMemberInfo.h
@@ -1,9 +1,47 @@
+
+
+/*
+ * GuildMemberList.h
+ *
+ *  Created on: Oct 27, 2010
+ *      Author: crush
+ */
+
+#ifndef GUILDMEMBERLIST_H_
+#define GUILDMEMBERLIST_H_
+
+#include "engine/engine.h"
+
+#include "GuildMemberInfo.h"
+
+namespace server {
+ namespace zone {
+  namespace objects {
+   namespace guild {
+
+	class GuildMemberList : public VectorMap<uint64, GuildMemberInfo> {
+		public:
+
+		GuildMemberList() {
+			setNoDuplicateInsertPlan();
+		}
+	};
+   }
+  }
+ }
+}
+
+using namespace server::zone::objects::guild;
+
+
+#endif /* GUILDMEMBERLIST_H_ */
+
 /*
  * GuildMemberInfo.h
  *
  *  Created on: Oct 27, 2010
  *      Author: crush
- */
+ *
 
 #ifndef GUILDMEMBERINFO_H_
 #define GUILDMEMBERINFO_H_
@@ -65,12 +103,12 @@ namespace guild {
 			return true;
 		}
 
-		/*inline void addSerializableVariables() {
+		\*inline void addSerializableVariables() {
 			addSerializableVariable("playerID", &playerID);
 			addSerializableVariable("guildTitle", &guildTitle);
 			addSerializableVariable("permissions", &permissions);
 			addSerializableVariable("declaredAllegiance", &declaredAllegiance);
-		}*/
+		}*\
 
 		inline uint64 getPlayerID() {
 			return playerID;
