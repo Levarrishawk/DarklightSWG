a10a96ed6de79a9f243855d0765f8b76bddd5719
Skyyyr <skydeath227@yahoo.com>
2015-04-26 17:12:09 -0700

    Update PvpCommand.h
    test mode

--- MMOCoreORB/src/server/zone/objects/creature/commands/PvpCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/PvpCommand.h
@@ -1,1 +1,38 @@
 
+#ifndef PVPCOMMAND_H_
+#define PVPCOMMAND_H_
+
+#include "server/zone/objects/scene/SceneObject.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/managers/player/PlayerManager.h"
+
+class PvpCommand : public QueueCommand {
+public:
+
+	PvpCommand(const String& name, ZoneProcessServer* server)
+		: QueueCommand(name, server) {
+
+	}
+
+	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) {
+
+		if (!checkStateMask(creature))
+			return INVALIDSTATE;
+
+		if (!checkInvalidLocomotions(creature))
+			return INVALIDLOCOMOTION;
+			
+		PlayerObject* ghost = player->getPlayerObject();
+		
+		if (ghost->getFactionStatus() == FactionStatus::COVERT)
+			ghost->setFactionStatus(FactionStatus::OVERT);
+		if (ghost->getFactionStatus() == FactionStatus::OVERT)
+			ghost->setFactionStatus(FactionStatus::COVERT);
+
+		return doCombatAction(creature, target);
+	}
+
+};
+
+#endif
